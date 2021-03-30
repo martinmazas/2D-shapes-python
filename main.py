881b612ad119f17566fab7701f81aa2b5c591e41
martinmazas <mazasmartin@gmail.com>
2021-03-30 10:28:21 -0300

    clear button and functionality added

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from tkinter import *
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 # Global variables
-click_number = x0 = y0 = x1 = y1 = radius = 0
+click_number = x0 = y0 = x1 = y1 = x2 = y2 = x3 = y3 = radius = 0
 height = width = 700
 
 window = Tk()
@@ -23,10 +23,10 @@ canvas.create_image((width // 2, height // 2), image=img, state="normal")
 def main():
     # Draw a single pixel
     def draw_pixel(x, y):
-        global canvas, img
+        global img
         if x < 0 or y < 0:
             pass
-        img.put("white", (x, y))
+        img.put("#000", (x, y))
 
     # Draw a line
     def my_line(event):
@@ -61,6 +61,16 @@ def main():
     def activate_circle():
         canvas.grid(row=3, column=3)
         canvas.bind('<Button-1>', my_circle)
+
+    def activate_curve():
+        canvas.grid(row=3, column=3)
+        canvas.bind('<Button-1>', my_curve)
+
+    def activate_clear():
+        global canvas, img
+        canvas.delete(img)
+        img = PhotoImage(width=width, height=height)
+        canvas.create_image((width // 2, height // 2), image=img, state="normal")
 
     # Radius calculation
     def calculate_radius(xx0, xx1, yy0, yy1):
@@ -120,17 +130,49 @@ def main():
             # canvas.create_oval(x0-radius, y0-radius, x0+radius, y0+radius, width=7)
             click_number = 0
 
+    # def my_curve(xx0, yy0, xx1, yy1, xx2, yy2, xx3, yy3):
+    #     canvas.create_line(xx0, yy0, xx1, yy1)
+    #     canvas.create_line(xx2, yy2, xx3, yy3)
+    #     canvas.create_line(xx2, yy2, xx1, yy1)
+    def my_curve(event):
+        global click_number, x0, y0, x1, y1, x2, y2, x3, y3
+        if click_number == 0:
+            x0 = event.x
+            y0 = event.y
+            draw_pixel(x0, y0)
+            click_number = 1
+        elif click_number == 1:
+            x1 = event.x
+            y1 = event.y
+            draw_pixel(x1, y1)
+            click_number = 2
+        elif click_number == 2:
+            x2 = event.x
+            y2 = event.y
+            draw_pixel(x2, y2)
+            click_number = 3
+        elif click_number == 3:
+            x3 = event.x
+            y3 = event.y
+            draw_pixel(x3, y3)
+            click_number = 0
+
     # my_window = Tk()
 
     # my_canvas = Canvas(window, width=700, height=700, background='grey')
 
-    linebtn = Button(window, text="Draw Line", command=activate_line)
-    linebtn.grid(row=0, column=1)
-    linebtn.place(x=250, y=50)
-    # linebtn.bind('<Button-1>', my_line)
+    line_btn = Button(window, text="Draw Line", command=activate_line)
+    line_btn.grid(row=0, column=1)
+    line_btn.place(x=150)
     circle_btn = Button(window, text="Draw Circle", command=activate_circle)
     circle_btn.grid(row=0, column=1)
-    circle_btn.place(x=350, y=50)
+    circle_btn.place(x=250)
+    curve_btn = Button(window, text="Draw Curve", command=activate_curve)
+    curve_btn.grid(row=0, column=1)
+    curve_btn.place(x=350)
+    clear_btn = Button(window, text="Clear", command=activate_clear)
+    clear_btn.grid(row=0, column=1)
+    clear_btn.place(x=450)
 
     window.mainloop()
 
